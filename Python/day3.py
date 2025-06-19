cc86647c7708b1f1b2555c9f2ca0be123b1e8f8b
aernika <mn.olmedo4@gmail.com>
2025-06-19 23:52:15 +0800

    skibidi

--- Python/day3.py
+++ Python/day3.py
@@ -1,4 +1,13 @@
+'''
 def find_perimeter(width, length):
     return (width + length) * 2
 
 print(find_perimeter(10, 20))
+'''
+
+# Create a function that takes two strings as arguments and returns the number of times the first string (the single character) is found in the second string.
+
+def char_count(txt1, txt2):
+    return txt2.count(txt1)
+
+print(char_count("OH", "OHIO"))
